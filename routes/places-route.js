6b0fe1183277f1efdbbafd0c92d011d09a074667
Soheil Nikroo <soheiln1234@gmail.com>
2021-09-26 08:57:10 -0700

    handeling error

--- routes/places-route.js
+++ routes/places-route.js
@@ -19,12 +19,26 @@ const DUMMY_PLACES = [
 router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid;
   const place = DUMMY_PLACES.find((place) => place.id === placeId);
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provide id.");
+    error.code = 404;
+    throw error;
+  }
+
   res.json({ place });
 });
 
 router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
   const place = DUMMY_PLACES.find((place) => place.creator === userId);
+
+  if (!place) {
+    const error = new Error("Could not find a place for the provide user id.");
+    error.code = 404;
+    return next(error);
+  }
+
   res.json({ place });
 });
 
