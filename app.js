593c131d4e8c75e68fc99e90d9f64d355c9c4fd4
Soheil Nikroo <soheiln1234@gmail.com>
2021-09-27 02:19:58 -0700

    creating places schema

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-route");
 const usersRoutes = require("./routes/users-route");
@@ -26,4 +27,13 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occurred!" });
 });
 
-app.listen(5000);
+mongoose
+  .connect(
+    "mongodb+srv://soheil:soheilnikroo4002@cluster0.brbsh.mongodb.net/places?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
